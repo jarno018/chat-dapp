079bd56e42e2136db5f0fcff6ca7d55832194996
jarno018 <jarno.lannoo@hotmail.com>
2022-03-01 20:45:19 +0100

    Changed variable names

--- chat-dApp/src/routes/user.js
+++ chat-dApp/src/routes/user.js
@@ -3,19 +3,19 @@ import 'gun/sea';
 import { writable } from 'svelte/store';
 
 // Init the DB with relay/signaling server
-export const db = GUN({peers: ['http://10.129.36.97:8765/gun']});
+export const gun = GUN({peers: ['http://10.129.36.97:8765/gun']});
 
 // Make the user
-export const user = db.user().recall({sessionStorage: true});
+export const user = gun.user().recall({sessionStorage: true});
 
 // Export writable we can listen to
 export const username = writable('');
 
 // If the username changes, set it for the listeners
-user.get('alias').on((name) => username.set(name));
+gun.get('alias').on((name) => username.set(name));
 
 // If data changes
-db.on(async (event) => {
+gun.on(async (event) => {
   const username = await user.get('alias');
   username.set(username);
 
